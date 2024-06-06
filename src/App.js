b586b84bc5ac010dc6ebf2f6aa0d52e894803f93
Krish <Krishkumer150@gamil.com>
2024-06-06 23:55:14 +0530

    Woking

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/NavbarC/Navbar";
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
