8450c4608e100b72daa7913876197ea9e6c38dcd
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-07 18:15:58 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/NavbarC/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Section1 from "./components/Pages/Section1";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        
+        <Section1/>
       </BrowserRouter>
     </>
   );
---
+++ src/components/Pages/Section1.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../Pages/Section1.css";
+const Section1 = () => {
+  return (
+    <>
+      <section className="hero-head">
+        <div className="heading-1">
+          <h1>design studio</h1>
+        </div>
+        <div className="sub-heading">
+          <h2>Simple & Modern Homes</h2>
+        </div>
+        <div className="sub-para">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            voluptates autem nesciunt architecto iusto, adipisci provident sed
+            nihil! Et, illo?
+          </p>
+        </div>
+        <div>
+          <button className="btn">
+            <span>learn more</span>
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Section1;
