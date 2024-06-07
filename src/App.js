4120abc757f13442d4b30ab5d06a086651984683
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-07 19:29:18 +0100

    late-night

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./components/NavbarC/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import Section1 from "./components/Pages/Section1";
-
+import ServiceSection from "./components/Pages/Section2";
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Section1/>
+        <ServiceSection/>
       </BrowserRouter>
     </>
   );
---
+++ src/components/Pages/Section2.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Pages/Section2.css"
+const ServiceSection = () => {
+  return (
+    <>
+      <div className="head-text">
+        <h4>WHAT WE DO</h4>
+        <h2>
+          We can build you the Interior <br />
+          of your dream
+        </h2>
+      </div>
+      <div className="main-container">
+        <div className="box-container"></div>
+        <div className="box-container"></div>
+        <div className="box-container"></div>
+      </div>
+    </>
+  );
+};
+
+export default ServiceSection;
